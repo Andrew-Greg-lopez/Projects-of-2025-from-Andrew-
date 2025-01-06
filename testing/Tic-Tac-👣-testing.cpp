4d52bd34fbb59998e67a2f277112900c156753d1
Andrew-Greg-lopez <andrewlopez472@yahoo.com>
2025-01-05 22:03:22 -0600

    Update Tic-Tac-👣-testing.cpp

--- testing/Tic-Tac-👣-testing.cpp
+++ testing/Tic-Tac-👣-testing.cpp
@@ -3,4 +3,5 @@ using namespace std;
 
 
 void displayBoard(char board [3][3]);
-bool checkWin(char board[3][3]. char player;
+bool checkWin(char board[3][3], char player);
+bool is draw(char board[3][3]);
