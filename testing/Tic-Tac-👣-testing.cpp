f78408a19b54fc6c5afa49ed25b72b6d9700e7e6
Andrew-Greg-lopez <andrewlopez472@yahoo.com>
2025-01-05 22:09:46 -0600

    Update Tic-Tac-👣-testing.cpp

--- testing/Tic-Tac-👣-testing.cpp
+++ testing/Tic-Tac-👣-testing.cpp
@@ -5,3 +5,11 @@ using namespace std;
 void displayBoard(char board [3][3]);
 bool checkWin(char board[3][3], char player);
 bool is draw(char board[3][3]);
+
+int main () {
+  char board[3][3] = {{'1', '2', '3'}
+                      {'4', '5', '6'}
+                      {'7', '8', '9'} };
+  char curretnPlayer = 'X';
+  int move;
+  bool gameWon = false;
