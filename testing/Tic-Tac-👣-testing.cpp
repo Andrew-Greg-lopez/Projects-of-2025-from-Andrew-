5b6bde790e3ddcee39d0857648a8af1cf06107ba
Andrew-Greg-lopez <andrewlopez472@yahoo.com>
2025-01-16 03:52:40 -0600

    Update Tic-Tac-👣-testing.cpp

--- testing/Tic-Tac-👣-testing.cpp
+++ testing/Tic-Tac-👣-testing.cpp
@@ -69,7 +69,7 @@ return true;
 return false;
 }
 
-bool isDraw(char baord[3][3]){
+bool isDraw(char board[3][3]){
   for (int i = 0; i < 3; i++) {
 for (int j = 0; j < 3; j++) {
 if (board[i][j] != 'X' && board[i][j] != 'O') {
