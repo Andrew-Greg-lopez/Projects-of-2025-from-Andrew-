fe13f402346e6c6b36157d814848975c283899ad
Andrew-Greg-lopez <andrewlopez472@yahoo.com>
2025-01-16 03:49:15 -0600

    Update Tic-Tac-👣-testing.cpp

--- testing/Tic-Tac-👣-testing.cpp
+++ testing/Tic-Tac-👣-testing.cpp
@@ -1,35 +1,34 @@
-#inlcude <iostream>
+#include <iostream>
 using namespace std;
 
 
-void displayBoard(char board [3][3]);
+void displayBoard(char board[3][3]);
 bool checkWin(char board[3][3], char player);
 bool isDraw(char board[3][3]);
 
-int main () {
-  char board[3][3] = {{'1', '2', '3'},
-                      {'4', '5', '6'},
-                      {'7', '8', '9'} };
-  char curretnPlayer = 'X';
-  int move;
-  bool gameWon = false;
+int main() {
+    char board[3][3] = { {'1', '2', '3'},
+                         {'4', '5', '6'},
+                         {'7', '8', '9'} };
+    char currentPlayer = 'X';
+    int move;
+    bool gameWon = false;
+  while (!gameWon && !isDraw(board)) {
+  displayBoard(board);
 
-  While (!gameWon && !isDraw(board)) {
-  dsplayBoard(bord);
-
-  court << "Player " << currentPlayer << ", enter your move (1-9): ";
+  cout << "Player " << currentPlayer << ", enter your move (1-9): ";
   cin >> move;
 
     int row = (move - 1) / 3;
     int col = (move - 1) % 3;
 
-    if (board[row][col] != 'X' && bopard[row][col] != 'O') {
+    if (board[row][col] != 'X' && board[row][col] != 'O') {
       board[row][col] = currentPlayer;
       gameWon = checkWin(board, currentPlayer);
 
-      currrentPlayer = (currentPlayer == 'X') ? 'O' : "X';
+      currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
         } else {
-        court<< "Invalid move! Try again.\n";
+        cout<< "Invalid move! Try again.\n";
     }
   }
 
@@ -41,7 +40,7 @@ int main () {
   }
       
 
- return0;
+ return 0;
 }
 
 void displayBoard(char board[3][3]){
@@ -57,7 +56,7 @@ cout << "\n";\
 
 bool checWin (char board[3][3], char player) {
 
-  for (int i = 0; i < 3, i++) {
+  for (int i = 0; i < 3; i++) {
     if ((board[i][0] == player && board[i][1] == player && board[i][2] == player) ||
       (board[0][i] == player && board[1][i] == player && board[2][i] == player)){
 return true;
@@ -71,7 +70,7 @@ return false;
 }
 
 bool isDraw(char baord[3][3]){
-  for (int j = 0; i < 3; i++) {
+  for (int i = 0; i < 3; i++) {
 for (int j = 0; j < 3; j++) {
 if (board[i][j] != 'X' && board[i][j] != 'O') {
 return false;
