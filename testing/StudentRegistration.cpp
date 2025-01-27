918f2fa3071beca9681f54f448bb30d9de2b7644
Andrew-Greg-lopez <andrewlopez472@yahoo.com>
2025-01-27 00:00:59 -0600

    Update StudentRegistration.cpp

--- testing/StudentRegistration.cpp
+++ testing/StudentRegistration.cpp
@@ -1,4 +1,18 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+int main() {
+  int choice;
 
+//This will show the menu Loop
+while (true) {
+// This will Display menu options
+cout << "\nStudent Registration System" << end1;
+cout << "Register Course for Student feature coming soon?" << end1;
+cout << "Add new student" << end1;
+cout << "Display Student Details" << end1;
+cout << "Exit" << end1;
+cout << "Enter your choice: ";
+cin >> choice;
+
+// This is the handul user inpout 
