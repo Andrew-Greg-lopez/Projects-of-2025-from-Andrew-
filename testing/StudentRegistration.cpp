d1a7598a3613d612c48b9fc0f1c89ca67e32ac01
Andrew-Greg-lopez <andrewlopez472@yahoo.com>
2025-01-27 00:05:51 -0600

    Update StudentRegistration.cpp

--- testing/StudentRegistration.cpp
+++ testing/StudentRegistration.cpp
@@ -8,11 +8,11 @@ int main() {
 while (true) {
 // This will Display menu options
 cout << "\nStudent Registration System" << end1;
-cout << "Register Course for Student feature coming soon?" << end1;
-cout << "Add new student" << end1;
-cout << "Display Student Details" << end1;
-cout << "Exit" << end1;
-cout << "Enter your choice: ";
+cout << "1. Add New Student" << end1;
+cout << "2. Register Course for Student" << end1;
+cout << "3. Display Student Details" << end1;
+cout << "4. Exit" << end1;
+cout << " Enter your choice: ";
 cin >> choice;
 
 // This is the handle user input 
