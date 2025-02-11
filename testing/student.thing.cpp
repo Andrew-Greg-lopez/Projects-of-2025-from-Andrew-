15c6dcd259b9e72073ee2d31e80cd7f5549c3532
Andrew-Greg-lopez <andrewlopez472@yahoo.com>
2025-02-11 01:05:49 -0600

    Update student.thing.cpp

--- testing/student.thing.cpp
+++ testing/student.thing.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string> 
+
+using namespace std;
+
+// main classes to store course name 
+class Course {
+public: 
+string courseName;
+
+Course(string name) {
+courseName= name;
+
 
